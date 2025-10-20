01cc616032984ee080bcd70e77f0c0af33ddecb6
Felipe Rodegher Tognetta <127894183+FelipeRTognetta@users.noreply.github.com>
2025-10-20 14:33:08 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 function Home() {
-    return 
-    <h1>
-        Larossa você é a luz azul que me guia, meu centro meu norte meu sul, 
-        quero construir uma fazenda isolada na cidade e viver o resto da minha vida com você, eu te amo S2.
-    </h1>
+    return
+    <>
+        <h1>
+            Larossa você é a luz azul que me guia, meu centro meu norte meu sul, 
+            quero construir uma fazenda isolada na cidade e viver o resto da minha vida com você, eu te amo S2.
+        </h1>
+        <h2>
+            Teste
+        </h2>
+    </>
 }
 
 export default Home;
