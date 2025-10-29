e15835ab4f303ce80be1c061667cbfa31dc68fe8
felipert <d.sis07@beneficiosocial.com.br>
2025-10-29 08:43:46 -0300

    em construção

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Larossa você é a luz azul que me guia, meu centro meu norte meu sul, quero
-      construir uma fazenda isolada na cidade e viver o resto da minha vida com
-      você, eu te amo S2.
-    </h1>
-  );
+  return <h1>Em construção TabMini</h1>;
 }
 
 export default Home;
