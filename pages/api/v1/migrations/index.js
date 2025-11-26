56711a6cc6d049afcf7ea312a4fed2bfc20d32f5
felipert <d.sis07@beneficiosocial.com.br>
2025-11-26 09:49:36 -0300

    quick fix

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -16,7 +16,7 @@ export default async function migrations(request, response) {
 
   if (request.method !== "POST" && request.method !== "GET") {
     await dbClient.end();
-    return response.status(405).send((content = "Method not allowed"));
+    return response.status(405);
   }
 
   if (request.method === "GET") {
